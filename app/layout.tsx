8bebd7aa03bd2274b6f79f428088172e43bf6dd0
buffybuu <115411375+buffybuu@users.noreply.github.com>
2026-03-31 17:41:09 +0700

    Create root layout for Next.js

---
+++ app/layout.tsx
@@ -0,0 +1,14 @@
+// Root layout for Next.js
+
+import { ReactNode } from 'react';
+
+const RootLayout = ({ children }: { children: ReactNode }) => {
+  return (
+    <html>
+      <head />
+      <body>{children}</body>
+    </html>
+  );
+};
+
+export default RootLayout;
